b496c9ef452b1c78a9dfed9c09a42be02d69aa26
lnVizzzi0n <artemsmolyakov29@gmail.com>
2024-02-02 23:29:35 +0300

    refactoring / bug fix

--- library/date_structurs/voidVector.c
+++ library/date_structurs/voidVector.c
@@ -1,6 +1,7 @@
 #include "voidVector.h"
 #include <string.h>
 
+//возвращает структуру-дескриптор вектор из n значений.
 vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
     vectorVoid result = {(int *) malloc(baseTypeSize * n), 0, n,
                          baseTypeSize};
@@ -11,6 +12,7 @@ vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
     return result;
 }
 
+//изменяет количество памяти, выделенное под хранение элементов вектора.
 void reserveV(vectorVoid *v, const size_t newCapacity) {
     if (newCapacity == 0)
         v->data = NULL;
@@ -26,32 +28,45 @@ void reserveV(vectorVoid *v, const size_t newCapacity) {
     v->capacity = newCapacity;
 }
 
+//освобождает память, выделенную поднеиспользуемые элементы.
 void shrinkToFitV(vectorVoid *v) {
     reserveV(v, v->size);
 }
 
+//удаляет элементы из контейнера, но не освобождает выделенную память.
 void clearV(vectorVoid *v) {
     v->size = 0;
 }
 
+//освобождает память, выделенную вектору.
 void deleteVectorV(vectorVoid *v) {
     free(v->data);
 }
 
+//проверка на то, является ли вектор пустым.
 bool isEmptyV(vectorVoid *v) {
     return v->size == 0;
 }
+
+//проверка на то, является ли вектор полным.
 bool isFullV(vectorVoid *v) {
     return v->size == v->capacity;
 }
+
+//возвращает i-ый элемент вектора v.
 void getVectorValueV(vectorVoid *v, size_t index, void *destination) {
     char *source = (char *) v->data + index * v->baseTypeSize;
     memcpy(destination, source, v->baseTypeSize);
 }
+
+//записывает на index-ый элемент вектора v значение, расположенное
+//по адресу source.
 void setVectorValueV(vectorVoid *v, size_t index, void *source) {
     char *destination = (char *) v->data + index * v->baseTypeSize;
     memcpy(destination, source, v->baseTypeSize);
 }
+
+//удаляет последний элемент из вектора.
 void popBackV(vectorVoid *v) {
     if (isEmptyV(v)) {
         fprintf(stderr, "The vector is empty, we cannot fulfill your request");
@@ -59,6 +74,8 @@ void popBackV(vectorVoid *v) {
     }
     v->size--;
 }
+
+//добавляет элемент x в конец вектора v.
 void pushBackV(vectorVoid *v, void *source) {
     if (v->capacity == 0)
         reserveV(v, 1);
